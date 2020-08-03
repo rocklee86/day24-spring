0f930b85e7f7509d2822c568ab4d4743e73f00a7
lixi <lixi@1000phone.com>
2020-08-03 11:40:27 +0800

    lixi86 1st project

--- src/main/java/cn/qphone/spring/TestDemo2.java
+++ src/main/java/cn/qphone/spring/TestDemo2.java
@@ -2,6 +2,7 @@ package cn.qphone.spring;
 
 public class TestDemo2 {
     public static void main(String[] args) {
+        System.out.println("lixi86");
         System.out.println("rocklee86");
     }
 }
