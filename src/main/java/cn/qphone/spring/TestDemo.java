446a6c16110497915aa1917492121af60bf22224
lixi <lixi@1000phone.com>
2020-08-03 11:29:33 +0800

    lixi86 1st java project

--- src/main/java/cn/qphone/spring/TestDemo.java
+++ src/main/java/cn/qphone/spring/TestDemo.java
@@ -2,6 +2,6 @@ package cn.qphone.spring;
 
 public class TestDemo {
     public static void main(String[] args) {
-        System.out.println("Helloworld");
+        System.out.println("Helloworld!!!");
     }
 }
